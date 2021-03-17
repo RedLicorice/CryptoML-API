d2d3f50cae59ba42484021980620a8264dafd568
Giuseppe <p3pp1n0@gmail.com>
2021-03-17 22:37:37 +0100

    Reduced grid search verbosity

--- app/grid_search.py
+++ app/grid_search.py
@@ -22,7 +22,7 @@ def main(dataset: str, target: str, pipeline: str, features: Optional[str] = Non
         logging.info("==[{}/{}]== MODEL: {} {} {} {} =====".format(i+1, len(search_models), m.symbol, m.dataset, m.target, m.pipeline))
         mp = tuning.create_parameters_search(m, split=0.7, features=features)
         logging.info("[{}] Start grid search".format(get_timestamp()))
-        mp = tuning.grid_search(m, mp, sync=True, verbose=1.5, n_jobs=8, halving=halving)
+        mp = tuning.grid_search(m, mp, sync=True, verbose=1, n_jobs=8, halving=halving)
         logging.info("[{}] End grid search".format(get_timestamp()))
 
 
