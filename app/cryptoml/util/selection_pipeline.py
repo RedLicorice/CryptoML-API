26f85b6b42830c2fb50a366e81f6dbb0ea94ba1e
Giuseppe <p3pp1n0@gmail.com>
2021-03-17 12:47:53 +0100

    Use Imblearn's pipeline

--- app/cryptoml/util/selection_pipeline.py
+++ app/cryptoml/util/selection_pipeline.py
@@ -1,9 +1,9 @@
 from sklearn.pipeline import Pipeline as SklearnPipeline
-
+from imblearn.pipeline import Pipeline as ImbLearnPipeline
 
 # This class exposes coef_ and feature_importances_ from a pipeline in order to
 #  use it for feature selection in wrapper methods such as RFECV or SelectFromModel
-class Pipeline(SklearnPipeline):
+class Pipeline(ImbLearnPipeline):
     def fit(self, X, y=None, **fit_params):
         super(Pipeline, self).fit(X, y, **fit_params)
         # We're assuming classifier is the last element of the pipeline
